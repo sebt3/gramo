aa2f3fc31ad65346e803edc9cceec6690fd4507b
Sébastien Huss <sebastien.huss@gmail.com>
2024-01-18 16:18:26 +0100

    Fix

--- back/resolvers/core/resolvers.ns.projectcalico.ts
+++
@@ -1,7 +0,0 @@
-
-import { resolver as NetworkPolicyNSresolver } from '../projectcalico/resolver.namespace.NetworkPolicy.js';
-import { resolver as NetworkSetNSresolver } from '../projectcalico/resolver.namespace.NetworkSet.js';
-export const resolvers = {
-    ...NetworkPolicyNSresolver,
-    ...NetworkSetNSresolver,
-};
