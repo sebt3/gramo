e4dc141e28b0060d16ed72fba5d9733d7f1f67bc
Sébastien Huss <sebastien.huss@gmail.com>
2024-01-20 14:53:31 +0100

    Fixes

--- front/routes/index.ts
+++ front/routes/index.ts
@@ -11,42 +11,42 @@ import { route as installRoute, link as installLink, descriptions as installDesc
 if (installRoute.children != undefined && installRoute.children?.length>0) {
   routeChildren.push(installRoute);
   links.push(installLink)
-  linkRedirect.path = installRoute.redirect!=undefined?installRoute.redirect.path:'/install'
+  linkRedirect.path = linkRedirect.path!=''?linkRedirect.path:installRoute.redirect!=undefined?installRoute.redirect.path:'/install'
 }
 
 import { route as automationRoute, link as automationLink, descriptions as automationDescriptions } from './automation'
 if (automationRoute.children != undefined && automationRoute.children?.length>0) {
   routeChildren.push(automationRoute);
   links.push(automationLink)
-  linkRedirect.path = automationRoute.redirect!=undefined?automationRoute.redirect.path:'/automation'
+  linkRedirect.path = linkRedirect.path!=''?linkRedirect.path:automationRoute.redirect!=undefined?automationRoute.redirect.path:'/automation'
 }
 
 import { route as databaseRoute, link as databaseLink, descriptions as databaseDescriptions } from './database'
 if (databaseRoute.children != undefined && databaseRoute.children?.length>0) {
   routeChildren.push(databaseRoute);
   links.push(databaseLink)
-  linkRedirect.path = databaseRoute.redirect!=undefined?databaseRoute.redirect.path:'/database'
+  linkRedirect.path = linkRedirect.path!=''?linkRedirect.path:databaseRoute.redirect!=undefined?databaseRoute.redirect.path:'/database'
 }
 
 import { route as configRoute, link as configLink, descriptions as configDescriptions } from './config'
 if (configRoute.children != undefined && configRoute.children?.length>0) {
   routeChildren.push(configRoute);
   links.push(configLink)
-  linkRedirect.path = configRoute.redirect!=undefined?configRoute.redirect.path:'/config'
+  linkRedirect.path = linkRedirect.path!=''?linkRedirect.path:configRoute.redirect!=undefined?configRoute.redirect.path:'/config'
 }
 
 import { route as networkRoute, link as networkLink, descriptions as networkDescriptions } from './network'
 if (networkRoute.children != undefined && networkRoute.children?.length>0) {
   routeChildren.push(networkRoute);
   links.push(networkLink)
-  linkRedirect.path = networkRoute.redirect!=undefined?networkRoute.redirect.path:'/network'
+  linkRedirect.path = linkRedirect.path!=''?linkRedirect.path:networkRoute.redirect!=undefined?networkRoute.redirect.path:'/network'
 }
 
 import { route as systemRoute, link as systemLink, descriptions as systemDescriptions } from './system'
 if (systemRoute.children != undefined && systemRoute.children?.length>0) {
   routeChildren.push(systemRoute);
   links.push(systemLink)
-  linkRedirect.path = systemRoute.redirect!=undefined?systemRoute.redirect.path:'/system'
+  linkRedirect.path = linkRedirect.path!=''?linkRedirect.path:systemRoute.redirect!=undefined?systemRoute.redirect.path:'/system'
 }
 
 export const descriptions = {
