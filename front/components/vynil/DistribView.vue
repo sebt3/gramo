451deb8479c877ee5c46d7a7f0cad306bfbff960
Sébastien Huss <sebastien.huss@gmail.com>
2024-01-21 02:07:55 +0100

    fixes

--- front/components/vynil/DistribView.vue
+++ front/components/vynil/DistribView.vue
@@ -3,11 +3,13 @@ import vynilDistribQuery from '@/queries/vynil/DistribView.graphql'
 import distribDelete from '@/queries/vynil/DistribDelete.graphql'
 import MetadataView from '../core/MetadataView.vue';
 import OpenApiEdit from '../core/OpenApiEdit.vue';
-import DefaultStatusView from '../core/DefaultStatusView.vue';
+import TableHeader from '../core/TableHeader.vue';
 import MonacoViewer from '../core/MonacoViewer.vue';
-import { useQuery, useMutation, useDistrib, getProperties } from './Distrib.js'
-const { viewer, viewerUpdate, onErrorHandler, notifySuccess, notifyError, onNotDistribFound, navigation, setItemFromRoute, toEdit, actionDelete } = useDistrib();setItemFromRoute();
-const { result, loading, onResult, onError } = useQuery(vynilDistribQuery, { "name": navigation.currentItem }, { pollInterval: 500 });onError(onErrorHandler); onResult(res => {onNotDistribFound(res);viewerUpdate(res, res.loading?{}:res.data.vynilDistrib.metadata.obj)});
+import { ref, useQuery, useMutation, useDistrib, packageColumns, getProperties } from './Distrib.js'
+let filter = ref('');
+const { viewer, viewerUpdate, onErrorHandler, notifySuccess, notifyError, onNotDistribFound, navigation, pagination, setItemFromRoute, toEdit, actionDelete } = useDistrib();setItemFromRoute();
+const { result, loading, onResult, onError } = useQuery(vynilDistribQuery, { "name": navigation.currentItem }, { pollInterval: 500 });onError(onErrorHandler);
+onResult(res => {onNotDistribFound(res);viewerUpdate(res, res.loading?{}:res.data.vynilDistrib.metadata.obj)});
 const { mutate: deletor, onDone: onDeleteDone, onError: onDeleteError } = useMutation(distribDelete);
 onDeleteDone(() => {
   notifySuccess('Deletion proceded');
@@ -19,7 +21,7 @@ onDeleteError((err) => {
 </script>
 <template>
   <div class="row q-mb-sm q-ml-sm">
-    <div class="col-md-6">
+    <div class="col-md-4">
       <q-card bordered v-if="!loading && result!=undefined && result.vynilDistrib!=undefined && result.vynilDistrib!=null" class="q-ma-sm">
         <q-card-section class="bg-primary text-grey-4">
           <div class="text-h6 q-mt-none q-mb-none q-pt-none q-pb-none">Distrib
@@ -37,17 +39,32 @@ onDeleteError((err) => {
           <MetadataView :metadata="result.vynilDistrib.metadata" />
         </q-card-section>
       </q-card>
+    </div><div class="col-md-4">
       <q-card bordered v-if="!loading && result!=undefined && result.vynilDistrib!=undefined && result.vynilDistrib!=null" class="q-ma-sm">
         <q-card-section>
           <div class="text-h6 text-grey-8 q-mt-none q-mb-none q-pt-none q-pb-none">Status</div>
         </q-card-section>
-        <q-card-section v-if="!loading && result.vynilDistrib!=null && result.vynilDistrib.status != null">
-          <DefaultStatusView :status="result.vynilDistrib.status" />
+        <q-card-section>
+          <div class="q-gutter-md">
+            <q-field label="Status" stack-label borderless>
+              <template v-slot:prepend><q-icon name="done" /></template>
+              <template v-slot:control>
+                <q-chip class="float-right text-white text-capitalize" label="Ok" color="positive" v-if="result.vynilDistrib.status.errors.length==0"></q-chip>
+                <q-chip class="float-right text-white text-capitalize" label="Errors" color="negative" v-if="result.vynilDistrib.status.errors.length>0"></q-chip>
+              </template>
+            </q-field>
+            <q-field label="Errors" stack-label borderless v-if="result.vynilDistrib.status.errors.length>0">
+              <template v-slot:prepend><q-icon name="error" /></template>
+              <template v-slot:control><div class="self-center full-width no-outline" tabindex="0">
+                <div v-for="err in result.vynilDistrib.status.errors" v-bind:key="err">{{ err }}</div>
+              </div></template>
+            </q-field>
+          </div>
         </q-card-section>
       </q-card>
-    </div><div class="col-md-6">
+    </div><div class="col-md-4">
       <q-card bordered v-if="!loading && result!=undefined && result.vynilDistrib!=undefined && result.vynilDistrib!=null" class="q-ma-sm">
-        <q-tabs v-model="viewer.tab" class="bg-primary text-grey-4" active-color="white">
+        <q-tabs v-model="viewer.tab" class="bg-primary text-white">
           <q-tab label="Options" name="simple" />
           <q-tab label="Specifications" name="spec" />
           <q-tab label="full Yaml" name="yaml" />
@@ -71,4 +88,10 @@ onDeleteError((err) => {
       </q-card>
     </div>
   </div>
+  <q-card v-if="!loading && result!=undefined && result.vynilDistrib!=undefined && result.vynilDistrib!=null" class="q-ma-sm">
+    <q-card-section>
+      <TableHeader title="Packages" v-model:model-filter="filter" itemtype='Package' :usecreate="false" :userefresh="false" />
+      <q-table :rows="result.vynilDistrib.packages" :columns="packageColumns" class="no-shadow" v-model:pagination="pagination" :filter="filter" hide-bottom></q-table>
+    </q-card-section>
+  </q-card>
 </template>
